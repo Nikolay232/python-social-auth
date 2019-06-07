a83362c42bfd8fce4822cd80890f0c58e8d0667c
Denis Kuchin <d.kuchin@eastwoodlab.ru>
2019-06-07 18:36:14 +0300

    BRRND-1563 Add logging

--- social/__init__.py
+++ social/__init__.py
@@ -2,6 +2,6 @@
 python-social-auth application, allows OpenId or OAuth user
 registration/authentication just adding a few configurations.
 """
-version = (0, 1, 25)
-extra = '-hh9'
+version = (0, 1, 26)
+extra = '-hh10'
 __version__ = '.'.join(map(str, version)) + extra
--- social/backends/hhru.py
+++ social/backends/hhru.py
@@ -12,8 +12,12 @@ Then update your settings values using registration information
 
 import urllib2
 import json
+import logging
 
 from social.backends.oauth import BaseOAuth2
+
+logger = logging.getLogger('social.auth')
+
 
 class HhruOAuth2(BaseOAuth2):
     """HH.ru OAuth2 support"""
@@ -34,9 +38,14 @@ class HhruOAuth2(BaseOAuth2):
 
     def user_data(self, access_token, response, *args, **kwargs):
         """Loads user data from service"""
-        request = urllib2.Request("http://api.hh.ru/me",
-                                  headers={"Authorization": "Bearer {token}".format(token=access_token),
-                                           "User-Agent": self.get_user_agent()})
+        try:
+            request = urllib2.Request("http://api.hh.ru/me",
+                                      headers={"Authorization": "Bearer {token}".format(token=access_token),
+                                               "User-Agent": self.get_user_agent()})
+        except HTTPError as ex:
+            logger.error("HTTPError. Token: {0}. Reason: {1}".format(access_token, ex))
+            raise
+            
         data = urllib2.urlopen(request).read()
 
         return json.loads(data)
